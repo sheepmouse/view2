55266e5076382299a24bb0b5ee89bb4366d7f652
sheepmouse <51688161+sheepmouse@users.noreply.github.com>
2023-11-23 18:01:49 +0800

    Delete static directory

--- static/js/chunk-2d0b2952.757b88ec.js
+++
@@ -1,7 +0,0 @@
-/*!
- *  build: vue-admin-better 
- *  vue-admin-better.com 
- *  https://gitee.com/chu1204505056/vue-admin-better 
- *  time: 2023-11-23 17:52:06
- */
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b2952"],{2570:function(n,t,c){"use strict";c.r(t);var e=function(){var n=this;n._self._c;return n._m(0)},u=[function(){var n=this,t=n._self._c;return t("div",[t("h3",[n._v("...待开发")])])}],r={},s=r,i=c("2877"),o=Object(i["a"])(s,e,u,!1,null,"5be1c440",null);t["default"]=o.exports}}]);
--- static/js/chunk-2d0c268e.efcc9185.js
+++
@@ -1,7 +0,0 @@
-/*!
- *  build: vue-admin-better 
- *  vue-admin-better.com 
- *  https://gitee.com/chu1204505056/vue-admin-better 
- *  time: 2023-11-23 17:52:06
- */
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c268e"],{"49d7":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this;n._self._c;return n._m(0)},u=[function(){var n=this,e=n._self._c;return e("div",[e("h3",[n._v("...待开发")])])}],r={},s=r,a=t("2877"),i=Object(a["a"])(s,c,u,!1,null,"a9479ea8",null);e["default"]=i.exports}}]);
